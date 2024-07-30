71d4f3e0e0d5c3cb6e1b72beaf2698e011e38517
Abdi Esayas <99608238+abdiesu04@users.noreply.github.com>
2024-07-30 22:54:53 +0300

    updated getAlbumByID

--- DAY-02/web-service-gin/main.go
+++ DAY-02/web-service-gin/main.go
@@ -13,41 +13,31 @@ type album struct {
     Price  float64 `json:"price"`
 }
 
-// albums slice to seed record album data.
 var albums = []album{
     {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
     {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
     {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
 }
 
-// getAlbums responds with the list of all albums as JSON.
 func getAlbums(c *gin.Context) {
     c.IndentedJSON(http.StatusOK, albums)
 }
 
-// postAlbums adds an album from JSON received in the request body.
 func postAlbums(c *gin.Context) {
     var newAlbum album
 
-    // Call BindJSON to bind the received JSON to
-    // newAlbum.
     if err := c.BindJSON(&newAlbum); err != nil {
         return
     }
 
-    // Add the new album to the slice.
     albums = append(albums, newAlbum)
     c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
 
-// getAlbumByID locates the album whose ID value matches the id
-// parameter sent by the client, then returns that album as a response.
 func getAlbumByID(c *gin.Context) {
     id := c.Param("id")
 
-    // Loop over the list of albums, looking for
-    // an album whose ID value matches the parameter.
     for _, a := range albums {
         if a.ID == id {
             c.IndentedJSON(http.StatusOK, a)
