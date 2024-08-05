3b7c1e2323325d0417a74e91bc3b9d9e0b45b20a
abdiesu04 <abdiesayasbayisa@gmail.com>
2024-08-05 18:12:06 +0300

    Configured the routes

--- DAY-03/task_manager/router/router.go
+++ DAY-03/task_manager/router/router.go
@@ -8,23 +8,32 @@ import (
     "go.mongodb.org/mongo-driver/mongo"
 )
 
+
 func SetupRouter(taskCollection *mongo.Collection, userCollection *mongo.Collection) *gin.Engine {
     r := gin.Default()
+
+    // Create task and user services.
     taskService := data.NewTaskService(taskCollection)
     userService := &data.UserService{Collection: userCollection}
 
-    // Register task and user services as middleware
+    // Register task and user services as middleware.
     r.Use(func(c *gin.Context) {
         c.Set("taskService", taskService)
         c.Set("userService", userService)
         c.Next()
     })
 
+    // Register API endpoints for user registration and authentication.
     r.POST("/register", controllers.Register)
     r.POST("/login", controllers.Login)
 
+    // Group authorized endpoints.
     authorized := r.Group("/")
+
+    // Require authentication for authorized endpoints.
     authorized.Use(middleware.AuthMiddleware())
+
+    // Register API endpoints for task management.
     {
         authorized.POST("/tasks", controllers.NewTaskController(taskService).CreateTask)
         authorized.GET("/tasks", controllers.NewTaskController(taskService).GetTasks)
@@ -33,5 +42,6 @@ func SetupRouter(taskCollection *mongo.Collection, userCollection *mongo.Collect
         authorized.DELETE("/tasks/:id", controllers.NewTaskController(taskService).DeleteTask)
     }
 
+    // Return the configured router.
     return r
 }
